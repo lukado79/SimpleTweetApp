44b1627a3d0097ea0638e26e0115a6c2a4dd6646
Łukasz Izdebski <lukaszizdebski@gazeta.pl>
2018-08-21 13:36:58 +0200

    new changes

---
+++ src/main/java/pl/coderslab/controller/LoginController.java
@@ -0,0 +1,52 @@
+package pl.coderslab.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import pl.coderslab.entity.User;
+import pl.coderslab.entity.UserRole;
+import pl.coderslab.service.UserRoleService;
+import pl.coderslab.service.UserService;
+
+@Controller
+public class LoginController {
+	
+	@Autowired
+	private UserService userService;
+	
+	@Autowired
+	private UserRole userRole;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+
+	@GetMapping("/login")
+	public String login(Model model) {
+
+		if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated()
+				&& !(SecurityContextHolder.getContext().getAuthentication() instanceof AnonymousAuthenticationToken)) {
+			return "redirect:/home";
+
+		}
+		model.addAttribute(new User());
+		return "login";
+
+	}
+
+	@PostMapping("/register")
+	public String register(@ModelAttribute User user, BindingResult result) {
+		userRole.setUserRole("user");
+		user.setPassword(passwordEncoder.encode(user.getPassword()));
+		user.setRole(userRole);
+		userService.addUsers(user, result);
+		return "home";
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/login.jsp
@@ -0,0 +1,38 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+	pageEncoding="UTF-8"%>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@	taglib prefix="form" uri="http://www.springframework.org/tags/form"%>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Insert title here</title>
+<link rel="stylesheet"
+	href="https://bootswatch.com/4/cosmo/bootstrap.min.css">
+</head>
+<body>
+	<div>
+		<h2>Login</h2>
+		<form method="post" action="/login">
+			<input type="text" name="username" placeholder="Enter username">
+			<input type="password" name="password" placeholder="Enter password">
+			<input type="hidden" name="${_csrf.parameterName}"
+				value="${_csrf.token}" /> <input type="submit" value="Log in">
+		</form>
+	</div>
+
+	<div>
+		<h2>Register</h2>
+		<form:form action="/register" method="post" modelAttribute="user">
+			<input path="firstName" placeholder="Enter firstname" />
+			<input path="lastName" placeholder="Enter lastname" />
+			<input path="email" placeholder="Enter email" />
+			<input path="password" placeholder="Enter password" type="password" />
+			<input path="role" value="user" type="hidden" />	
+			<input type="hidden" name="${_csrf.parameterName}"
+				value="${_csrf.token}" />
+			<input type="submit" value="Register">
+		</form:form>
+	</div>
+</body>
+</html>
