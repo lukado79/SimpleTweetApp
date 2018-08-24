fea68f3d648aca7320a051c49835f10cadf8d941
Łukasz Izdebski <lukaszizdebski@gazeta.pl>
2018-08-24 13:03:54 +0200

    update

--- src/main/java/pl/coderslab/controller/CommentsController.java
+++ src/main/java/pl/coderslab/controller/CommentsController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import pl.coderslab.entity.Comments;
 import pl.coderslab.service.CommentsService;
@@ -20,15 +22,22 @@ public class CommentsController {
 	@Autowired
 	CommentsService commentsService;
 
+	@GetMapping("/comment/add")
 	public String addComment(Model model) {
 		return commentsService.addComment(model);
 
 	}
-	
+	@PostMapping("/comment/add")
 	public String addComment(@Valid @ModelAttribute Comments comment, BindingResult result, @PathVariable long id, Principal principal) {
 		return commentsService.addComment(comment, result, id, principal);
 	}
 	
+	@GetMapping("/tweet/all")
+	public String allComments(Model model) {
+		return commentsService.findAllComments(model);
+	}
+	
+	@GetMapping("/comment/delete/{id}")
 	public String deleteComment(@PathVariable long id) {
 		return commentsService.deleteComment(id);
 	}
