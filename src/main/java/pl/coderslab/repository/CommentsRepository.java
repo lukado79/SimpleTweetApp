8f129fb77b1abebaa1c23ca1e210a9731d595664
Łukasz Izdebski <lukaszizdebski@gazeta.pl>
2018-11-18 21:47:40 +0100

    still spring security error

--- src/main/java/pl/coderslab/repository/CommentsRepository.java
+++ src/main/java/pl/coderslab/repository/CommentsRepository.java
@@ -4,10 +4,11 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import pl.coderslab.entity.Comments;
-
+@Repository
 @Transactional
 public interface CommentsRepository extends JpaRepository<Comments, Long> {
 
--- src/main/java/pl/coderslab/repository/UserRepository.java
+++ src/main/java/pl/coderslab/repository/UserRepository.java
@@ -3,10 +3,12 @@ package pl.coderslab.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
 
 import pl.coderslab.entity.User;
 
+@Repository
 @Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
 
