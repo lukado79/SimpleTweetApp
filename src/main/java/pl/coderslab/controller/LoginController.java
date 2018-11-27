63aa351bcb76fae42b782224ec5ae6985a71abf1
Łukasz Izdebski <lukaszizdebski@gazeta.pl>
2018-11-27 23:28:45 +0100

    tuesday commit

--- src/main/java/pl/coderslab/controller/LoginController.java
+++ src/main/java/pl/coderslab/controller/LoginController.java
@@ -4,12 +4,17 @@ import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import pl.coderslab.entity.User;
 
 @Controller
 public class LoginController {
+	
+
 
 	@GetMapping("/login")
 	public String login(HttpSession sess, Model model) {
@@ -20,6 +25,16 @@ public class LoginController {
 	@PostMapping("login")
 	public String login(@RequestParam String username, @RequestParam String password, HttpSession sess, Model model) {
 		return "index";
+	}
+
+	@GetMapping("/register")
+	public String register(Model model) {
+		return "register";
+	}
+
+	@PostMapping("/registger")
+	public String register(@ModelAttribute User user, BindingResult result) {
+		return "login";
 	}
 
 }
